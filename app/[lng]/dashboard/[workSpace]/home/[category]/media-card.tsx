83fb36d41f654a9f9f64f2acb1f1ef13f31b52c1
wave-ys <yujiasun01@outlook.com>
2023-11-24 21:00:07 +0800

    add music cover

--- app/[lng]/dashboard/[workSpace]/home/[category]/media-card.tsx
+++ app/[lng]/dashboard/[workSpace]/home/[category]/media-card.tsx
@@ -2,6 +2,7 @@ import {MediaModel} from "@/request/media";
 import {TimeDistance} from "@/components/date-display";
 import {cn} from "@/lib/utils";
 import Image from "next/image";
+import musicCover from './music_cover.jpeg'
 
 export interface MediaCardProps {
   media: MediaModel;
@@ -18,7 +19,8 @@ export default function MediaCard({media, lng, className}: MediaCardProps) {
       className
     )}>
       <div className={"h-52 flex-none relative"}>
-        <Image sizes={"26rem, 32rem"} priority className={"object-cover"} fill src={`/api/thumbnail/${media.id}`}
+        <Image sizes={"26rem, 32rem"} priority className={"object-cover"} fill
+               src={media.fileType === 'video' ? `/api/thumbnail/${media.id}` : musicCover}
                alt={media.fileName}/>
       </div>
       <div className={"bg-white/10 flex-auto p-2 space-y-2"}>
