4dc1a12c9035c9439edc51f6442ab8dd4817a282
wave-ys <yujiasun01@outlook.com>
2023-12-19 18:44:36 +0800

    change default theme

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout(
     )}>
     <ThemeProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="dark"
       enableSystem
     >
       <TooltipProvider>
