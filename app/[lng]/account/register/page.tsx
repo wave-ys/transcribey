5781d9d2c464fb5f9b38b5f350ff4608243f690e
wave-ys <yujiasun01@outlook.com>
2023-12-23 21:29:33 +0800

    register page init

---
+++ app/[lng]/account/register/page.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image"
+import {UserAuthForm} from "@/app/[lng]/account/register/user-auth-form";
+import {ThemeToggle} from "@/components/ui/theme-toggle";
+import {useTranslation} from "@/app/i18n";
+import {LanguageToggle} from "@/components/ui/language-toggle";
+
+export interface LoginPageProps {
+  params: {
+    lng: string
+  }
+}
+
+export default async function LoginPage({params: {lng}}: LoginPageProps) {
+  const {t} = await useTranslation(lng);
+  return (
+    <div
+      className="container relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <span className={"absolute right-4 top-4 md:right-8 md:top-8 flex items-center space-x-2"}>
+          <ThemeToggle/>
+          <LanguageToggle/>
+        </span>
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        <div className="absolute inset-0 bg-zinc-900"/>
+        <div className="relative z-20 flex items-center text-lg font-medium">
+          <Image className={"mr-2"} src={"/favicon.ico"} alt={"icon"} width={24} height={24}/>
+          Transcribey
+        </div>
+      </div>
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              {t("signUp.title")}
+            </h1>
+          </div>
+          <UserAuthForm/>
+        </div>
+      </div>
+    </div>
+  )
+}
--- app/[lng]/account/register/user-auth-form.tsx
+++ app/[lng]/account/register/user-auth-form.tsx
@@ -7,8 +7,6 @@ import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {useTranslation} from "@/app/i18n/client";
-import {ImGithub} from "react-icons/im";
-import {Checkbox} from "@/components/ui/checkbox";
 import {useSearchParams} from "next/navigation";
 import {ExclamationTriangleIcon} from "@radix-ui/react-icons";
 import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
@@ -23,7 +21,7 @@ export function UserAuthForm({className, ...props}: UserAuthFormProps) {
   return (
     <div className={cn("grid gap-6", className)} {...props}>
       <form action={"/api/auth/log-in?useCookies=true"} method={"post"}>
-        <div className="grid gap-2">
+        <div className="grid gap-2 mb-4">
           {search.get("failed") === 'true' &&
               <Alert variant="destructive" className={"grid gap-1"}>
                   <ExclamationTriangleIcon className="h-4 w-4"/>
@@ -56,46 +54,14 @@ export function UserAuthForm({className, ...props}: UserAuthFormProps) {
               type="password"
             />
           </div>
-          <div className="grid gap-1">
-            <div className={"flex justify-between"}>
-              <div className={"flex items-center space-x-1"}>
-                <Checkbox
-                  id="rememberMe"
-                  name={"rememberMe"}
-                />
-                <Label htmlFor="rememberMe">
-                  {t("login.rememberMe")}
-                </Label>
-              </div>
-              <Button type={"button"} variant={"link"}>{t("login.forgetPassword")}</Button>
-            </div>
-          </div>
-          <Button>
-            {t("login.signInButton")}
-          </Button>
-          <a href={"/account/register"}>
-            <Button className={"w-full"} variant={"outline"} type={"button"}>
-              {t("login.signUpButton")}
-            </Button>
-          </a>
         </div>
-      </form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t"/>
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            {t("login.orContinueWith")}
-          </span>
-        </div>
-      </div>
-      <a href={"/api/auth/external-login?provider=github"}>
-        <Button className={"w-full"} variant="outline" type="button">
-          <ImGithub className="mr-2 h-4 w-4"/>
-          Github
+        <Button className={"w-full"}>
+          {t("signUp.signUpButton")}
         </Button>
-      </a>
+        <a href={"/account/login"}>
+          <Button className={"w-full"} type={"button"} variant={"link"}>{t("signUp.haveAccount")}</Button>
+        </a>
+      </form>
     </div>
   )
 }
